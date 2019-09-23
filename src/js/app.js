7a6e5a561e84a6e6ed4d170bc98eb407a796d452
Alex Beuscher <alexbeuscher@gmail.com>
2019-09-22 21:58:37 -0700

    Tidied up examples

--- src/js/app.js
+++ src/js/app.js
@@ -3,14 +3,12 @@ var loginScreen = require("./pug-templates/login.pug");
 var parseHTML = require("./utils/parse-html.js");
 
 window.addEventListener("load", function(e) {
-    console.log("We're loaded and good to go");
-    console.log("A second thing!");
-    var el = document.getElementById("login");
-    console.log(el);
-    el.appendChild(parseHTML(loginScreen()));
+
+    var el = document.getElementById("app");
+
     fetch("http://beuscher.net/wp-json/wp/v2/webpage/")
         .then(response => response.json())
-        .then(function(data) {
-            console.log(data[0].title.rendered);
+        .then(response => {
+            el.appendChild(parseHTML(loginScreen(response[0])));
         });
 });
